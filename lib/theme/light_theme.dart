20f02a7207209e2dafd8415d1424f3ce43f5a9e5
Tetstt <120795261+YursenIndustries@users.noreply.github.com>
2023-08-13 10:35:36 +0200

    ???

--- lib/theme/light_theme.dart
+++ lib/theme/light_theme.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 
 ThemeData lightTheme = ThemeData(
-    brightness: Brightness.light,
-    appBarTheme: AppBarTheme(
-      backgroundColor: Colors.grey[900],
-    ),
-    colorScheme: ColorScheme.light(
-      background: Colors.grey[300]!,
-      primary: Colors.grey[200]!,
-      secondary: Colors.grey[300]!,
-    ),
-    textButtonTheme: TextButtonThemeData(
-        style: TextButton.styleFrom(foregroundColor: Colors.blue)));
+  brightness: Brightness.light,
+  appBarTheme: AppBarTheme(
+    backgroundColor: Colors.grey[900],
+  ),
+  colorScheme: ColorScheme.light(
+    background: Colors.grey[300]!,
+    primary: Colors.grey[200]!,
+    secondary: Colors.grey[300]!,
+  ),
+  textButtonTheme: TextButtonThemeData(
+    style: TextButton.styleFrom(foregroundColor: Colors.blue),
+  ),
+);
