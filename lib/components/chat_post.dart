b1f4bd4163f75b4a25b8f29084537e024b208e63
Tetstt <120795261+YursenIndustries@users.noreply.github.com>
2023-08-18 09:09:17 +0200

    updated the light theme

--- lib/components/chat_post.dart
+++ lib/components/chat_post.dart
@@ -2,7 +2,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:nexus/components/delet_button.dart';
 
 class ChatPosts extends StatefulWidget {
   final String message;
